73f9eb668904c4b4c5bd11f25dce94dc403f4db9
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-01 18:14:22 +0700

    Add files via upload

--- stl.sh
+++ stl.sh
@@ -1,24 +1,79 @@
 #!/bin/bash
 #stl (Wegare)
-            number_of_clients=$(grep -c '^# BEGIN_SSSO' /etc/akun/akun-ss.txt)
-			if [[ "$number_of_clients" = 0 ]]; then
-				echo -e "There are no existing clients!"
-				exit
-			fi
-			echo -e "Select the client to remove:"
-			grep '^# BEGIN_SSSO' /etc/akun/akun-ss.txt | cut -d ' ' -f 3 | nl -s ') '
-			read -p "Pilih No Client: " client_number
-			until [[ "$client_number" =~ ^[0-9]+$ && "$client_number" -le "$number_of_clients" ]]; do
-			    echo -e "$client_number: invalid selection."
-				read -p "Pilih No Client: " client_number
-			done
-			client=$(grep '^# BEGIN_SSSO' /etc/akun/akun-ss.txt | cut -d ' ' -f 3 | sed -n "$client_number"p)
-				# Remove from the configuration file
-				sed -i "/^# BEGIN_SSSO $client/,/^# END_SSSO $client/d" /etc/akun/akun-ss.txt
-				pid="$(ps -ef | grep ss-server | grep /etc/shadowsocks-libev/${client}.json | awk '{print $2}' | tail -n1)"
-				kill -9 "$pid"
-				sed -i "/^# BEGIN_SSSO $client/,/^# END_SSSO $client/d" /etc/crontab
-				sed -i "/^# BEGIN_SSSO $client/,/^# END_SSSO $client/d" /etc/rc.local
-				rm -r /etc/shadowsocks-libev/"$client".json
-				echo -e "$client removed!"
-				
+echo "Inject SSL by wegare"
+echo "1. Sett Profile"
+echo "2. Start Inject"
+echo "3. Stop Inject"
+echo "4. Enable booting"
+echo "5. Disable booting"
+read -p "(default tools: 2):" tools
+[ -z "${tools}" ] && tools="2"
+if [ "$tools" = "1" ]; then
+mkdir -p ~/akun/
+mkdir -p ~/.ssh/
+touch ~/akun/ssl.conf
+host2="$(cat ~/akun/ssl.conf | grep -i connect | head -n1 | cut -d: -f1 | sed 's/ //g')" 
+route del $host2 gw 192.168.8.1 metric 4
+read -p "Masukkan host/ip" host
+read -p "Masukkan port" port
+read -p "Masukkan bug" bug
+read -p "Masukkan port udpgw" udp
+read -p "Masukkan user" user
+read -p "Masukkan pass" pass
+echo "[SSH]
+client = yes
+accept = localhost:69
+connect = $host:$port
+sni = $bug" > ~/akun/ssl.conf
+echo "#udpgw=$udp
+#pass=$pass
+Host ssl*
+    PermitLocalCommand yes
+    LocalCommand gproxy %h
+    DynamicForward 1080
+    StrictHostKeyChecking no
+    ServerAliveInterval 10
+    TCPKeepAlive yes
+Host ssl1
+    HostName 127.0.0.1
+    Port 69
+    User $user" > ~/.ssh/config
+ip tuntap add dev tun0 mode tun
+ifconfig tun0 10.0.0.1 netmask 255.255.255.0 up
+route add $host gw 192.168.8.1 metric 4
+route add default gw 10.0.0.2 metric 6
+echo "Sett Profile Sukses"
+sleep 0.5
+clear
+stl
+elif [ "${tools}" = "2" ]; then
+
+stunnel ssl.conf
+sshpass -p $pass ssh -N ssl1 &
+badvpn-tun2socks --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp &
+elif [ "${tools}" = "3" ]; then
+gproxy stop && killall stunnel && killall ssh
+echo "Stop Suksess"
+elif [ "${tools}" = "4" ]; then
+sed -i 's/exit 0/ /g' /etc/rc.local
+host="$(cat ~/akun/ssl.conf | grep -i connect | head -n1 | cut -d: -f1 | sed 's/ //g')" 
+pass="$(cat ~/.ssh/config | grep -i pass | cut -d= -f2)" 
+udp="$(cat ~/.ssh/config | grep -i udpgw | cut -d= -f2)" 
+echo "# BEGIN STL
+ip tuntap add dev tun0 mode tun
+ifconfig tun0 10.0.0.1 netmask 255.255.255.0 up
+route add $host gw 192.168.8.1 metric 4
+route add default gw 10.0.0.2 metric 6
+stunnel ssl.conf
+sshpass -p $pass ssh -N ssl1 &
+badvpn-tun2socks --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp &
+# END STL
+exit 0" >> /etc/rc.local
+echo "Enable Suksess"
+elif [ "${tools}" = "5" ]; then
+sed -i "/^# BEGIN STL/,/^# END STL/d" /etc/rc.local
+echo "Disable Suksess"
+else 
+echo -e "$tools: invalid selection."
+exit
+fi
