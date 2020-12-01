6298f7e0323400b8befec2ee393486b62b9473c2
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-02 04:19:29 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ opkg update && opkg install ip-full && opkg install openssh-client && opkg insta
 killall stunnel
 /etc/init.d/redsocks stop
 sed -i 's/exit 0/ /g' /etc/rc.local
-ip tuntap add dev tun1 mode tun
-ifconfig tun1 10.0.0.1 netmask 255.255.255.0 up
-echo "ip tuntap add dev tun1 mode tun
-ifconfig tun1 10.0.0.1 netmask 255.255.255.0 up
+ip tuntap add dev tun0 mode tun
+ifconfig tun0 10.0.0.1 netmask 255.255.255.0
+echo "ip tuntap add dev tun0 mode tun
+ifconfig tun0 10.0.0.1 netmask 255.255.255.0
 exit 0" >> /etc/rc.local
 echo "base {
     log_debug = off;
