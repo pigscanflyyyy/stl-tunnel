e5edc27532f20c84850568c6729f24e237496b9e
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-03 02:17:26 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 wget -O ~/badvpn.ipk "https://github.com/wegare123/stl-tunnel/blob/main/badvpn_1.999.130-1_aarch64_cortex-a53.ipk?raw=true"
 wget -O /usr/bin/stl "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/stl.sh"
 wget -O /usr/bin/stl-start "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/stl-start.sh"
+wget -O /usr/bin/gproxy "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/gproxy.sh"
+wget -O /etc/redsocks.conf "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/redsocks.conf"
 opkg update && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install redsocks && opkg install badvpn.ipk
 /etc/init.d/stunnel disable
 /etc/init.d/redsocks disable
