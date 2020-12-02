14f5fb4c2cc5e5034d69dd11a85ffa64212dede1
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-03 02:14:39 +0700

    Add files via upload

---
+++ gproxy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#stl (Wegare)
+udp="$(cat ~/.ssh/config | grep -i udpgw | cut -d= -f2)" 
+host="$(cat ~/akun/ssl.conf | grep -i connect | head -n1 | awk '{print $3}' | cut -d: -f1)" 
+route="$(route -n | grep -i 192 | head -n1 | awk '{print $2}')" 
+case $1 in
+"stop")
+	iptables -t nat -F REDSOCKS
+	iptables -t nat -F OUTPUT
+	iptables -t nat -F PREROUTING
+	echo "Internet Disconnected"
+	killall redsocks
+	exit
+;;
+esac
+    echo ""
+    iptables -t nat -N REDSOCKS
+    #Ignore LANs and some other reserved addresses.
+    iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
+    iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+    iptables -t nat -A REDSOCKS -d 10.10.1.0/22 -j RETURN
+    iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
+    iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
+    iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
+    iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
+    iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
+    iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+    iptables -t nat -A REDSOCKS -d 202.152.240.50/32 -j RETURN
+    iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+    iptables -t nat -A PREROUTING -d 192.168.0.0/16 -j RETURN
+    iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports 12345
+    iptables -t nat -A OUTPUT -j REDSOCKS
+    redsocks -c /etc/redsocks.conf > /dev/null &
+    screen -d -m badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp &
+    route add 1.1.1.1 gw $route metric 4
+    route add 1.0.0.1 gw $route metric 4
+    route add $host gw $route metric 4
+    route add default gw 10.0.0.2 metric 6
+    sleep 5
+    echo "Internet Connected"
+
+
+
