3b0b43b4dbb0928623d4dab1ca084de2d64bf14f
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-02 12:53:32 +0700

    Add files via upload

--- stl.sh
+++ stl.sh
@@ -54,7 +54,7 @@ route="$(route -n | grep -i 192 | head -n1 | awk '{print $2}')"
 stunnel ~/akun/ssl.conf
 sshpass -p $pass ssh -N ssl1 &
 sleep 35
-badvpn-tun2socks --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp &
+screen -d -m badvpn-tun2socks --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp &
 route add 1.1.1.1 gw $route metric 4
 route add 1.0.0.1 gw $route metric 4
 route add $host gw $route metric 4
